2c58f10de74e4d1b97203e974e22210e95bcf548
huangganggui <huangganggui>
2023-06-25 14:28:01 +0800

    chage adsense style

--- components/Ads/GoogleAdSense.tsx
+++ components/Ads/GoogleAdSense.tsx
@@ -10,13 +10,13 @@ export const GoogleAdSense = () => {
         client="ca-pub-216573955128325"
         slot="8740003054"
 
-        // style={{ display: 'block' }}
-        // format="auto"
-        // responsive="true"
-
-        style={{ width: 224, height: 100 }}
-        format=""
+        style={{ display: 'block' }}
+        format="auto"
         responsive="true"
+
+        // style={{ width: 224, height: 100 }}
+        // format=""
+        // responsive="true"
 
         // style={{ display: 'block' }}
         // layout="in-article"
